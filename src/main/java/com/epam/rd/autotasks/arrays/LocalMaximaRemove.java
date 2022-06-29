afeed69c4410d03b932f5d3aecae4364cb536092
Pavel Kulikov <ppkulikov@gmail.com>
2022-06-29 09:22:19 +0300

    SOLVED

--- src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
+++ src/main/java/com/epam/rd/autotasks/arrays/LocalMaximaRemove.java
@@ -5,15 +5,31 @@ import java.util.Arrays;
 public class LocalMaximaRemove {
 
     public static void main(String[] args) {
-        int[] array = new int[]{18, 1, 3, 6, 7, -5};
-
+        int[] array = new int[]{15, 15, 15, 15, 15, 15, 15};
         System.out.println(Arrays.toString(removeLocalMaxima(array)));
     }
 
-    public static int[] removeLocalMaxima(int[] array){
+    public static int[] removeLocalMaxima(int[] array)throws UnsupportedOperationException{
+        int [] arrayWoLm = new int [array.length];
+        int j =0;
+        for (int i=0;i < array.length-1; i++){
+            if (array[i] <= array[i+1]){
+                arrayWoLm[j] = array[i];
+                j++;
+            } else if(i != 0 && array[i] == array[i-1]){
+                arrayWoLm[j] = array[i];
+                j++;
+            }
+        }
+        if (array[array.length-1] <= array[array.length-2]){
+            arrayWoLm[j] = array[array.length-1];
+            j++;
+        }
+        arrayWoLm = Arrays.copyOf(arrayWoLm,j);
+        return arrayWoLm;
 
         //put your code here
 
-        throw new UnsupportedOperationException();
+
     }
 }
